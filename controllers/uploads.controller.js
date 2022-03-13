0951b11ba920aca200511d752ec7e9b86b190a0a
Hugo Colin <colin.hugo.89@gmail.com>
2022-03-13 13:49:22 -0600

    Se agregaron las rutas que permiten ver las imagenes de noticias, prevenciones y usuarios

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,7 +1,7 @@
 const path = require( 'path' );
 const fs = require( 'fs' );
 
-const { Mensaje } = require( '../models' );
+const { Mensaje, Noticia, Prevencion, Usuario } = require( '../models' );
 
 const getImagenMensaje = async ( req, res ) => {
 
@@ -27,15 +27,117 @@ const getImagenMensaje = async ( req, res ) => {
 
     catch ( error ){
 
-        console.error( 'Error al mostrar la imagen.', error );
+        console.error( 'Error al mostrar la imagen de mensajes.', error );
 
         return res.json( {
             value: 0,
-            msg: 'Error al mostrar la imagen.'
+            msg: 'Error al mostrar la imagen de mensajes.'
+        } );
+    }
+}
+
+const getImagenNoticia = async ( req, res ) => {
+
+    const { idNoticia } = req.params;
+
+    const noticia = await Noticia.findById( idNoticia );
+    
+    try {
+
+        if ( noticia.foto ){
+            
+            const pathImagen = path.join( __dirname, '../uploads/noticias/', noticia.foto );
+
+            if ( fs.existsSync( pathImagen ) ){
+                return res.sendFile( pathImagen );
+            }
+        }
+
+        const pathImagen = path.join( __dirname, '../assets/no-image.jpg' );
+
+        return res.sendFile( pathImagen );
+    }
+
+    catch ( error ){
+
+        console.error( 'Error al mostrar la imagen de noticias.', error );
+
+        return res.json( {
+            value: 0,
+            msg: 'Error al mostrar la imagen de noticias.'
+        } );
+    }
+}
+
+const getImagenPrevencion = async ( req, res ) => {
+
+    const { idPrevencion } = req.params;
+
+    const prevencion = await Prevencion.findById( idPrevencion );
+    
+    try {
+
+        if ( prevencion.foto ){
+            
+            const pathImagen = path.join( __dirname, '../uploads/prevenciones/', prevencion.foto );
+
+            if ( fs.existsSync( pathImagen ) ){
+                return res.sendFile( pathImagen );
+            }
+        }
+
+        const pathImagen = path.join( __dirname, '../assets/no-image.jpg' );
+
+        return res.sendFile( pathImagen );
+    }
+
+    catch ( error ){
+
+        console.error( 'Error al mostrar la imagen de prevenciones.', error );
+
+        return res.json( {
+            value: 0,
+            msg: 'Error al mostrar la imagen de prevenciones.'
+        } );
+    }
+}
+
+const getImagenUsuario = async ( req, res ) => {
+
+    const { idUsuario } = req.params;
+
+    const usuario = await Usuario.findById( idUsuario );
+    
+    try {
+
+        if ( usuario.foto ){
+            
+            const pathImagen = path.join( __dirname, '../uploads/usuarios/', usuario.foto );
+
+            if ( fs.existsSync( pathImagen ) ){
+                return res.sendFile( pathImagen );
+            }
+        }
+
+        const pathImagen = path.join( __dirname, '../assets/no-image.jpg' );
+
+        return res.sendFile( pathImagen );
+    }
+
+    catch ( error ){
+
+        console.error( 'Error al mostrar la imagen de usuarios.', error );
+
+        return res.json( {
+            value: 0,
+            msg: 'Error al mostrar la imagen de usuarios.'
         } );
     }
 }
 
 module.exports = {
     getImagenMensaje,
+    getImagenNoticia,
+    getImagenPrevencion,
+    getImagenUsuario
 }
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,11 +1,12 @@
 const router = require( 'express' ).Router();
 const { check } = require( 'express-validator' );
 
-const { existeMensaje } = require('../helpers');
+const { existeMensaje, existeNoticia, existePrevencion, existeUsuario } = require('../helpers');
 
 const { validarCampos } = require( '../middlewares' );
 
-const { getImagenMensaje } = require( '../controllers/uploads.controller' );
+const { getImagenMensaje, getImagenNoticia,
+        getImagenPrevencion, getImagenUsuario } = require( '../controllers/uploads.controller' );
 
 // **********************************************************************
 // *******************     M E N S A J E S *****************************
@@ -16,5 +17,35 @@ router.get( '/mensajes/:idMensaje', [
     check( 'idMensaje' ).custom( existeMensaje ),
     validarCampos
 ], getImagenMensaje );
+
+// **********************************************************************
+// *******************  N O T I C I A S *****************************
+// **********************************************************************
+
+router.get( '/noticias/:idNoticia', [
+    check( 'idNoticia', 'No es un id válido.' ).isMongoId(),
+    check( 'idNoticia' ).custom( existeNoticia ),
+    validarCampos
+], getImagenNoticia );
+
+// **********************************************************************
+// ******************     P R E V E N C I O N     **********************
+// **********************************************************************
+
+router.get( '/prevenciones/:idPrevencion', [
+    check( 'idPrevencion', 'No es un id válido.' ).isMongoId(),
+    check( 'idPrevencion' ).custom( existePrevencion ),
+    validarCampos
+], getImagenPrevencion );
+
+// **********************************************************************
+// *******************  U S U A R I O *****************************
+// **********************************************************************
+
+router.get( '/usuarios/:idUsuario', [
+    check( 'idUsuario', 'No es un id válido.' ).isMongoId(),
+    check( 'idUsuario' ).custom( existeUsuario ),
+    validarCampos
+], getImagenUsuario );
 
 module.exports = router;
