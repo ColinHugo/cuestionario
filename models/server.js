97eb99ec552ca54b3cdcfbabef15689850492655
Hugo Colin <colin.hugo.89@gmail.com>
2022-03-14 09:20:19 -0600

    Se sirven la carpeta contenedora de las imagenes

--- models/server.js
+++ models/server.js
@@ -37,6 +37,7 @@ class Server{
         this.app.use( cors() );
         this.app.use( express.json( { limit: '100mb' } ) );
         this.app.use( mongoSanitize() );
+        this.app.use( express.static( __dirname + '/../uploads' ) );
     }
 
     routes(){
