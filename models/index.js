4a0a3b243dc10ea8c315ad219ac6bf52926b09dc
Hugo Colin <colin.hugo.89@gmail.com>
2022-03-16 11:16:59 -0600

    Se quito la coleccion de rol, no se utilizaba

--- models/index.js
+++ models/index.js
@@ -2,7 +2,6 @@ const Mensaje = require( './mensaje' );
 const Noticia = require( './noticia' );
 const Pregunta = require( './pregunta' );
 const Prevencion = require( './prevencion' );
-const Rol = require( './rol' );
 const Temperatura = require( './temperatura' );
 const Usuario = require( './usuario' );
 const Visita = require( './visita' );
@@ -12,7 +11,6 @@ module.exports = {
     Noticia,
     Pregunta,
     Prevencion,
-    Rol,
     Temperatura,
     Usuario,
     Visita,
