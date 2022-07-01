6ea219782d0bdbb690e78146cf50b379feaaf3d6
Hugo Colin <colin.hugo.89@gmail.com>
2022-06-30 23:08:42 -0500

    Archivo que se encarga de verificar las fechas y sumarle un dia a la fecha hasta

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const validaCampos  = require( './validar-campos' );
+const validarFechas = require('./validar-fechas');
 const validaJWT  = require( './validar-jwt' );
 
 module.exports = {
     ...validaCampos,
+    ...validarFechas,
     ...validaJWT
 }
