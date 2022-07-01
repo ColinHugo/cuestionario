9e892dd13eedb78ad77f7095dadb4d470586b506
Hugo Colin <colin.hugo.89@gmail.com>
2022-06-30 23:03:36 -0500

    Se crea en automatico la carpeta contenedora de las imagenes

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
 require( 'dotenv' ).config();
+
 const Server = require( './models/server' );
 
 const server = new Server();
+
+if ( !fs.existsSync( path.join( __dirname, 'uploads' ) ) ) {
+    fs.mkdirSync( 'uploads' );
+}
+
 server.listen();
