ae79ce13d7922e592d35105370df595dab1da165
Hugo Colin <colin.hugo.89@gmail.com>
2022-06-30 23:09:42 -0500

    Se anadio el middleware para verificar las fechas

--- routes/reportes.routes.js
+++ routes/reportes.routes.js
@@ -3,15 +3,16 @@ const { check } = require( 'express-validator' );
 
 const { existeUsuario } = require( '../helpers' );
 
-const { validarCampos } = require( '../middlewares' );
+const { validarCampos, validarFechas } = require( '../middlewares' );
 
 const { getReportes, getReporteById } = require( '../controllers/reportes.controller' );
 
-router.get( '/:desde/:hasta/:seccion', getReportes );
+router.get( '/:desde/:hasta/:seccion', validarFechas, getReportes );
 
 router.get( '/:idUsuario/:desde/:hasta/:seccion', [
     check( 'idUsuario', 'No es un id válido.' ).isMongoId(),
     check( 'idUsuario' ).custom( existeUsuario ),
+    validarFechas,
     validarCampos
 ], getReporteById );
 
